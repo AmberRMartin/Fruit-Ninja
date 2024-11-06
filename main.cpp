85ef91c52a7f832e379c48949c56bda4a08d35ed
Nicholas Cicala <ncicala@students.nic.edu>
2024-11-06 14:46:16 -0800

    Reverted Main back to old main

--- main.cpp
+++ main.cpp
@@ -5,39 +5,8 @@ Add #includes as you see fit
 
 */
 #include <iostream>
-#include "stats.h"
 
-int main ()
+int main()
 {
-    Stats stats;
-    Stats stats2;
-    std::cout << "Entity 1 - HP: " << stats.getHP() 
-            << "/" << stats.getMaxHP() << " ATK: " 
-            << stats.getATK() << " LV: " 
-            << stats.getLevel() << std::endl;
-    std::cout << "Entity 2 - HP: " << stats2.getHP() 
-            << "/" << stats2.getMaxHP() << " ATK: " 
-            << stats2.getATK() << " LV: " 
-            << stats2.getLevel() << std::endl;
-
-    stats.reduceHP(stats2.getATK());
-
-    std::cout << "Entity 1 - HP: " << stats.getHP() 
-            << "/" << stats.getMaxHP() << " ATK: " 
-            << stats.getATK() << " LV: " 
-            << stats.getLevel() << std::endl;
-
-    stats2.levelUp();
-
-    std::cout << "Entity 2 - HP: " << stats2.getHP() 
-            << "/" << stats2.getMaxHP() << " ATK: " 
-            << stats2.getATK() << " LV: " 
-            << stats2.getLevel() << std::endl;
-
-    stats.reduceHP(999);
-
-    std::cout << "Entity 1 - HP: " << stats.getHP() 
-            << "/" << stats.getMaxHP() << " ATK: " 
-            << stats.getATK() << " LV: " 
-            << stats.getLevel() << std::endl;
+    std::cout << "This is main. There is nothing here yet\n";
 }
