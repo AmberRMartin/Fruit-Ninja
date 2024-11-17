24340585423b9253c07d9fcaf84f07ec151c3f14
AmberRMartin <armartin3@nic.edu>
2024-11-17 15:41:32 -0800

    Fixed main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 //  * @brief Create and play a small turn-based rpg called Fruit Ninja
 //  * @version 0.1
 //  * @date 2024-11-03
-//  * 
+//  * @compile  g++ -std=c++11 *.cpp -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
 //  * @copyright Copyright (c) 2024
 //  * 
 //  */
@@ -13,53 +13,81 @@
 // // This the main.cpp file
 // // Add #includes as you see fit
 
-// #include "mainMenu.h"
-// #include "button.h"
-// #include "game.h"
-// #include <SFML/Graphics.hpp>
+#include "game.h"
+#include <SFML/Graphics.hpp>
 
-// int main()
-// {
-//     sf::RenderWindow window(sf::VideoMode(900,500), "Fruit Ninja!");
-//     Game game;
-//     MainMenu mainMenu({450, 250},{500,500}); //Position, size
-//     Button mStart("Start", {550,250}, {154,55});
-//     Button mExit("Exit", {550, 340}, {154,55});
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(900,500), "Fruit Ninja!");
+    Game game;
 
 
-//     while(window.isOpen())
-//     {
-//         sf::Event event;
-//         while(window.pollEvent(event))
-//         {
-//             if(event.type == sf::Event::Closed)
-//             {
-//                 window.close();
-//             }
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
 
-//             window.clear();
+            window.clear();
 
-// //Main menu
-//             if(game.startmenu == false) //Do not do a while loop here. You WILL get trapped
-//             {
+//Main menu
+            if(game.startmenu == false) //Do not do a while loop here. You WILL get trapped
+            {
 
-//                 window.draw(mainMenu);
-//                 window.draw(mStart);
-//                 window.draw(mExit);
+                window.draw(game.mainMenu);
+                window.draw(game.mainMenu.mStart);
+                window.draw(game.mainMenu.mExit);
 
-//                 if(mStart.clicked(event,window))
-//                 {
-//                     game.startmenu = true;
-//                 }
-//                 else if(mExit.clicked(event,window))
-//                 {
-//                     window.close();
-//                 }
-//             }
-//         }
-// //Note: putting window.clear(); over here at the bottom makes the images flash. 
-// //Leave window.close() where it is unless you can fix it
-//         window.display();
-//     }
+                if(game.mainMenu.mStart.clicked(event,window))
+                {
+                    game.startmenu = true;
+                }
+                else if(game.mainMenu.mExit.clicked(event,window))
+                {
+                    window.close();
+                }
+            }
 
-// }
+//Map screen
+            else if (game.pauseMenu.isPaused == false && game.inventoryMenu.isOpen == false)
+            {
+
+/*
+Note for Matthew: This displays both pause and inventory button on screen.
+This is the only section where the player is supposed to be able to move around
+as the other two else if statements have one eof those menus open
+You may delete this message whenever you please
+*/
+                window.draw(game.pauseMenu);
+                game.pauseMenu.update(event, window);
+                window.draw(game.inventoryMenu);
+                game.inventoryMenu.update(event, window);
+            }
+//Pause screen
+            else if (game.pauseMenu.isPaused == true && game.inventoryMenu.isOpen == false)
+            {
+                window.draw(game.pauseMenu);
+                game.pauseMenu.update(event, window);
+            }
+
+//Inventory screen
+            else if (game.pauseMenu.isPaused == false && game.inventoryMenu.isOpen == true)
+            {
+
+                window.draw(game.inventoryMenu);
+                game.inventoryMenu.update(event, window);
+            }
+            
+        }
+
+
+//Note: putting window.clear(); over here at the bottom makes the images flash. 
+//Leave window.close() where it is unless you can fix it
+        window.display();
+    }
+
+}
